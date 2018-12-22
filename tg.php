e07924aaa8c3fb055c5ef54715887c2bdfaf1323
WheresMyMoneyBitch <46014079+WheresMyMoneyBitch@users.noreply.github.com>
2018-12-22 05:30:04 +0200

    Update tg.php

--- tg.php
+++ tg.php
@@ -1,6 +1,6 @@
 <?php
-$token   = ''; // from @BotFather /newbot
-$chat_id = ''; // from https://api.telegram.org/bot$token/getUpdates
+$token   = '346858347:AAE2gHDPUMKg55uWEKo9VRImSSVaAOqJVE0'; // from @BotFather /newbot
+$chat_id = '346858347'; // from https://api.telegram.org/bot$token/getUpdates
 $text = htmlspecialchars( $_POST['text'] );
 
 $sendToTelegram = fopen( 'https://api.telegram.org/bot' . $token . '/sendMessage?chat_id=' . $chat_id . '&parse_mode=html&text=' . $text, 'r' );
